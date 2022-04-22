706945f758f30319b19eb927f3e7022b3bfcfbe5
yash-deore <codemaninfinity17@gmail.com>
2022-04-23 00:48:59 +0530

    Wallet integration complete and polyfill error solved
    Wallet integration complete and polyfill error solved in react scripts webpack.config.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import WalletConnectButton from "./Components/WalletConnectButton";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <WalletConnectButton />
+    </div>
+  );
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { UseWalletProvider } from "use-wallet";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <UseWalletProvider>
+      <App />
+    </UseWalletProvider>
   </React.StrictMode>
 );
